fbbabba53df58f558e7a7c46d673079388707fd7
Raymond Cheng <me@raymondcheng.net>
2022-01-31 12:44:54 -0800

    Add Google Optimize to /marketing-optimize

--- pages/marketing-optimize.tsx
+++ pages/marketing-optimize.tsx
@@ -4,7 +4,11 @@ import {
   PlasmicComponent,
   ComponentRenderData
 } from '@plasmicapp/loader-nextjs';
+import Script from 'next/script'
 import { PLASMIC } from '../plasmic-init';
+
+const GA_OPTIMIZE_ID = 'OPT-M5TS8SR';
+const GA_TRACKING_ID = 'UA-154582048-2';
 
 // Statically fetch the data needed to render Plasmic pages or components.
 export const getStaticProps = async () => {
@@ -21,6 +25,19 @@ export const getStaticProps = async () => {
 export default function Page(props: { plasmicData: ComponentRenderData }) {
   return (
     <PlasmicRootProvider loader={PLASMIC} prefetchedData={props.plasmicData}>
+      <Script src={`https://www.googleoptimize.com/optimize.js?id=${GA_OPTIMIZE_ID}`}></Script>
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></Script>
+      <script
+        async
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${GA_TRACKING_ID}');
+          `,
+        }}
+      />
       <PlasmicComponent component="/marketing" />
     </PlasmicRootProvider>
   );
